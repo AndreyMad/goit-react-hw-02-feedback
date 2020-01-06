1a1c93c6987962ddd98f754de37e7a10cbfd69d8
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2020-01-06 13:17:44 +0200

    some commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
+    percentage: 0
   };
 
   changeStats = e => {
@@ -15,12 +16,16 @@ class App extends Component {
     }));
   };
 
-  feedbeckCounter = () => {
+  feedbackCounter = () => {
     return this.state.good + this.state.neutral + this.state.bad;
   };
 
+  positivePercentageCounter = () => {
+    this.setState.percentage = (this.state.good * 100) / this.feedbackCounter();
+  };
+
   render() {
-    const { good, neutral, bad } = this.state;
+    const { good, neutral, bad, percentage } = this.state;
     return (
       <div>
         <h1 className={style.title}>Please leave feedback</h1>
@@ -50,7 +55,12 @@ class App extends Component {
           <p className={style.stat_text}>Good: {good}</p>
           <p className={style.stat_text}>Neutral: {neutral}</p>
           <p className={style.stat_text}>Bad: {bad}</p>
-          <p>Total: {this.feedbeckCounter()}</p>
+          <p>Total: {this.feedbackCounter()}</p>
+          {percentage > 0 ? (
+            <p>Positive percentage: {percentage}</p>
+          ) : (
+            <p>No percentage</p>
+          )}
         </section>
       </div>
     );
